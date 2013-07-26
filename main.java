7f8049143759be15dd56bdd84483338d65e8651d
Alejandro Gallardo <ealexgg@gmail.com>
2013-07-26 00:55:57 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,52 @@
+package numerosfracciones;
+
+/**
+ *
+ * @author Alex Gallardo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Fraccion nf=new Fraccion();
+        Fraccion frac_x=new Fraccion();
+        Fraccion nftemp=new Fraccion();
+       int x,y,i;
+       String msg;
+       
+       x=Integer.parseInt(javax.swing.JOptionPane.showInputDialog("Dame el numerador:"));
+       y=Integer.parseInt(javax.swing.JOptionPane.showInputDialog("Dame el denominador:"));
+       nf=new Fraccion (x,y);
+       nf.mostrar();
+       nftemp=nf;
+       i=2;
+       msg=" ";
+       do {
+            frac_x=new Fraccion (1,i);
+            if (nftemp.compara(nftemp, frac_x))
+            {
+              nftemp=nftemp.resta(nftemp,frac_x) ;
+              msg=msg+frac_x.num+"/"+frac_x.den+"+ ";
+              javax.swing.JOptionPane.showMessageDialog(null,msg);
+              nftemp.reduce(nftemp);
+              }
+            i++;}
+            while(!(nftemp.escero()));
+            
+       
+    }
+}
+            
+            
+       
+       
+        
+      
+        
+   
+    
+        
+
+
